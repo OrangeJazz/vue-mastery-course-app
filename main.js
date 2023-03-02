6a0becb656cf06a069914ab353de889ecd21837e
OrangeJazz <wilinwen@mail.ru>
2023-03-03 01:12:23 +0400

    feat: lesson-6 end

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data: function () {
     return {
+      cart: 0,
       product: "Socks",
       description: "Funny socks with different colors",
       image: "./assets/images/socks_green.jpg",
@@ -10,9 +11,22 @@ const app = Vue.createApp({
       details: ["50% cotton", "30% wool", "20% polyester"],
       sizes: [37, 38, 39, 40, 41, 42, 43, 44],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
     };
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    deleteFromCart() {
+      if (this.cart > 0) this.cart -= 1;
+    },
+
+    updateImage(image) {
+      this.image = image;
+    },
   },
 });
