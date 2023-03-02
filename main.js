cca97f8b409549f6ac3f88005f03f8abae2ebda8
OrangeJazz <wilinwen@mail.ru>
2023-03-03 00:58:24 +0400

    feat: lesson-5 end

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
       inStock: true,
       inventory: 5,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: [37, 38, 39, 40, 41, 42, 43, 44],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
