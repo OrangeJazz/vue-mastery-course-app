b52b54b64eea3e0e6418b5190e07ad5cfa2de426
OrangeJazz <wilinwen@mail.ru>
2023-03-03 01:26:46 +0400

    feat: lesson-7 end

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
       description: "Funny socks with different colors",
       image: "./assets/images/socks_green.jpg",
       url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
-      inStock: true,
+      inStock: false,
       inventory: 5,
       details: ["50% cotton", "30% wool", "20% polyester"],
       sizes: [37, 38, 39, 40, 41, 42, 43, 44],
