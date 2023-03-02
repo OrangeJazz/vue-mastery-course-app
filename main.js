e0f992e1b5d5375198a5d2e016b18fb73bd88d3f
OrangeJazz <wilinwen@mail.ru>
2023-03-03 01:44:10 +0400

    feat: lesson-8 end

--- main.js
+++ main.js
@@ -3,16 +3,29 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       description: "Funny socks with different colors",
-      image: "./assets/images/socks_green.jpg",
+      // image: "./assets/images/socks_green.jpg",
+      selectedVariant: 0,
       url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
-      inStock: false,
+      // inStock: false,
       inventory: 5,
+      onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       sizes: [37, 38, 39, 40, 41, 42, 43, 44],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -25,8 +38,24 @@ const app = Vue.createApp({
       if (this.cart > 0) this.cart -= 1;
     },
 
-    updateImage(image) {
-      this.image = image;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+
+  computed: {
+    title() {
+      return `${this.brand} ${this.product}`;
+    },
+
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      return this.onSale ? `${this.title} is on sale` : null;
     },
   },
 });
