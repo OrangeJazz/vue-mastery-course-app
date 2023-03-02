96b3dd716487534dfaf47a87eb90b9cbb3747e89
OrangeJazz <wilinwen@mail.ru>
2023-03-03 00:41:35 +0400

    feat: lesson-3 end

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "Funny socks with different colors",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
     };
   },
 });
