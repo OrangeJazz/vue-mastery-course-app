f982d2cd26a3494e1642cc60829c8253e42613fd
OrangeJazz <wilinwen@mail.ru>
2023-03-03 00:49:33 +0400

    feat: lesson-4 end

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const app = Vue.createApp({
       description: "Funny socks with different colors",
       image: "./assets/images/socks_green.jpg",
       url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3",
+      inStock: true,
+      inventory: 5,
     };
   },
 });
